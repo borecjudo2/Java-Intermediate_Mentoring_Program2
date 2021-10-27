984f85bddc10327ec06027e6bec53c1fe91e07ad
Vladislav Karpeka <Vlad.Karpeka@vertexinc.com>
2021-10-27 23:08:24 +0300

    Implemented second and third tasks

--- elk/elk-spring-boot/src/main/java/com/epam/elkspringboot/service/impl/EventServiceImpl.java
+++ elk/elk-spring-boot/src/main/java/com/epam/elkspringboot/service/impl/EventServiceImpl.java
@@ -6,6 +6,7 @@ import com.epam.elkspringboot.repo.EventRepository;
 import com.epam.elkspringboot.service.EventService;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
@@ -19,6 +20,7 @@ import java.util.List;
  * @author Vladislav_Karpeka
  * @version 1.0.0
  */
+@Slf4j
 @Service
 @AllArgsConstructor
 public class EventServiceImpl implements EventService {
@@ -27,11 +29,13 @@ public class EventServiceImpl implements EventService {
 
   @Override
   public Event createEvent(Event event) {
+    log.info("createEvent");
     return repository.save(event);
   }
 
   @Override
   public List<Event> findAllEvents() {
+    log.info("findAllEvents");
     List<Event> events = new ArrayList<>();
     repository.findAll().forEach(events::add);
     return events;
@@ -39,23 +43,27 @@ public class EventServiceImpl implements EventService {
 
   @Override
   public List<Event> findAllByTitle(String title) {
+    log.info("findAllByTitle");
     return repository.findAllByTitle(title);
   }
 
   @Override
   public List<Event> findAllEventWhereTypeWorkshopByTitle(String title) {
+    log.info("findAllEventWhereTypeWorkshopByTitle");
     return repository.findAllByEventTypeAndTitle(EventType.WORKSHOP, title);
   }
 
   @SneakyThrows
   @Override
   public List<Event> findAllByDateTimeBeforeAndTitle(String dateTime, String title) {
+    log.info("findAllByDateTimeBeforeAndTitle");
     Date date = new SimpleDateFormat("dd.MM.yyyy").parse(dateTime);
     return repository.findAllByDateTimeBeforeAndTitle(date, title);
   }
 
   @Override
   public void deleteAll() {
+    log.info("deleteAll");
     repository.deleteAll();
   }
 }

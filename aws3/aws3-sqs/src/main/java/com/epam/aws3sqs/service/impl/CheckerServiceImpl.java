d0cb3c03f042814d0df828463156db98757cf23c
Vladislav Karpeka <Vlad.Karpeka@vertexinc.com>
2021-12-11 21:51:23 +0300

    AWS-3: Flow implemented

--- aws3/aws3-sqs/src/main/java/com/epam/aws3sqs/service/impl/CheckerServiceImpl.java
+++ aws3/aws3-sqs/src/main/java/com/epam/aws3sqs/service/impl/CheckerServiceImpl.java
@@ -1,7 +1,10 @@
 package com.epam.aws3sqs.service.impl;
 
 import com.epam.aws3.model.Order;
+import com.epam.aws3sqs.repo.OrderRepository;
 import com.epam.aws3sqs.service.CheckerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +14,17 @@ import org.springframework.stereotype.Service;
  * @author Vladislav_Karpeka
  * @version 1.0.0
  */
+@Slf4j
 @Service
+@AllArgsConstructor
 public class CheckerServiceImpl implements CheckerService {
+
+  private final OrderRepository orderRepository;
 
   @JmsListener(destination = "${activemq.order.sorted.name}")
   @Override
   public void receiveMessage(Order order) {
-    System.out.println(order);
+    orderRepository.save(order);
+    log.info("Order has been saved: " + order);
   }
 }
